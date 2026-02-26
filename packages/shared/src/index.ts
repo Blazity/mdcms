7a8c5e66c72d1ed9c76b0aeeb79dc3f7a1807f90
Karol Chudzik <iipandarx@protonmail.com>
2026-02-26 20:31:01 +0100

    refactor(shared): remove contracts barrel file
    Import directly from individual contract modules instead of
    re-exporting through lib/contracts/index.ts.

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./lib/shared.js";
 export * from "./lib/runtime/index.js";
-export * from "./lib/contracts/index.js";
+export * from "./lib/contracts/action-catalog.js";
+export * from "./lib/contracts/content-scope.js";
+export * from "./lib/contracts/extensibility.js";
--- packages/shared/src/lib/contracts/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./action-catalog.js";
-export * from "./content-scope.js";
-export * from "./extensibility.js";
